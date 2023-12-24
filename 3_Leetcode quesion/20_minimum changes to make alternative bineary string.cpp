439f163f79a0e642d2795932796d399847c658c8
prathmesh0125 <bidveprathmesh590@gmail.com>
2023-12-24 09:43:45 +0530

    practice

--- 3_Leetcode quesion/20_minimum changes to make alternative bineary string.cpp
+++ 3_Leetcode quesion/20_minimum changes to make alternative bineary string.cpp
@@ -4,33 +4,41 @@ int MinimumChanges(string s)
 {
   int altercount0 = 0;
   int altercount1 = 0;
-  for (int i = 0; i < s.size(); i++)
-  {
-    if (i % 2 == 0)
-    {
-      if (s[i] == '0')
-      {
-        altercount0++;
-      }
-      else
-      {
-        altercount1++;
-      }
-    }
-  else
-  {
+  // for (int i = 0; i < s.size(); i++)
+  // {
+  //   if (i % 2 == 0)
+  //   {
+  //     if (s[i] == '0')
+  //     {
+  //       altercount1++;
+  //     }
+  //     else
+  //     {
+  //       altercount0++;
+  //     }
+  //   }
+  // else
+  // {
 
-    if (s[i] == '1')
-    {
-      altercount1++;
-    }
-    else
-    {
-      altercount0++;
-    }
-  }
-  }
+  //   if (s[i] == '1')
+  //   {
+  //     altercount1++;
+  //   }
+  //   else
+  //   {
+  //     altercount0++;
+  //   }
+  // }
+  // }
 
+
+ for (int i = 0; i < s.size(); i++) {
+        if ((i % 2 == 0 && s[i] == '1') || (i % 2 == 1 && s[i] == '0')) {
+            altercount0++;
+        } else {
+            altercount1++;
+        }
+    }
   return min(altercount0, altercount1);
 }
 
